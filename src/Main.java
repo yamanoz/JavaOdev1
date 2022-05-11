f3f0df492fa5ecf2499d8cdd1679f139ab8e287d
Özgür Yaman <yamanozgur6333@gmail.com>
2022-05-11 23:10:22 +0300

    KDV li hesaplama pratiği yapıldı

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+
+        //noteHesaplama();
+        kdvHesaplama();
+
+    }
+    private static void kdvHesaplama() {
+        // Ödev: KDV Tutarı Hesaplama
+        // Kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran program
+
+        double tutar, kdvtutari, kdvOran;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Tutar Giriniz:");
+        tutar = input.nextDouble();
+        kdvOran=(tutar>0 && tutar<1000) ? 0.18 : 0.08;
+        double kdvfiyat = tutar + tutar * kdvOran;
+        double kdvtutar = kdvfiyat - tutar;
+
+        System.out.println("KDV'li tutar: " + kdvfiyat);
+        System.out.println("KDV Tutari: " + kdvtutar);
+
+    }
+
+    private static void noteHesaplama() {
         int matematik,kimya,fizik,turce,tarih,muzik,sonuc;
         Scanner inp=new Scanner(System.in);
 
@@ -19,9 +42,6 @@ public class Main {
         kimya=inp.nextInt();
         sonuc=(matematik+kimya+fizik+tarih+turce+muzik)/6;
         System.out.println(sonuc>60 ? "Geçti":"Kaldı");
-
-
-
     }
 
 
